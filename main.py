36a811395aa20b73ca86750e836051dcf5f2cbb9
viliam-gago <60181358+viliam-gago@users.noreply.github.com>
2022-07-21 20:35:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+# while loop
+
+# # zakladni pripad
+# i = 2
+# while i<20:
+#     print(i)
+#     print('ahoj')
+#     i += 1
+
+
+# ## while loop s vnorenym slozitym kodem
+# index = 0
+# while index < 1000:
+#     if len(str(index)) == 3:
+#         print(index)
+#         index = index + 1
+#     else:
+#         index = index + 1
+
+
+
+## while loop s else
+
+# index = 10
+# while index < 20:
+#     print(index)
+#     index = index + 1
+#
+#     if index == 15:
+#         break
+#
+# else:
+#     print("-"*23, "Podminka -> False", "-"*23)
+#
+# print('Pokracuju pod smyckou')
+
+
+
+
+while True:
+    uzivatelsky_vstup = input("zadej cislo: ")
+    if uzivatelsky_vstup.isnumeric():
+        print('Super, dik!')
+        break
+    else:
+        print('Toto neni cislo !')
+
+vysledek = int(uzivatelsky_vstup) + 5
+print('')
+print('Vysledek je', str(vysledek))
+
+
+# print("ehgrehrwth".isnumeric())
+# "auto"
+# print("123".isnumeric())
+
+
+
+
+
+
